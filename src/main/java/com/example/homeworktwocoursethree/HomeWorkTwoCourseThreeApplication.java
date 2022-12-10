eb66b474f6d1b8b7cc8fbece72a1ed0fab9a6780
Linar Daminov <linar.daminov@yandex.ru>
2022-12-10 23:12:49 +0500

    FirstCommit

---
+++ src/main/java/com/example/homeworktwocoursethree/HomeWorkTwoCourseThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworktwocoursethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkTwoCourseThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkTwoCourseThreeApplication.class, args);
+    }
+
+}
